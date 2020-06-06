bdaefc457301f7abab04af2afbeb3603d10d4229
Maciek Golowkin <maciek.golowkin@gmail.com>
2020-06-07 00:34:09 +0200

    Dodanie paska nawigacyjnego

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { ApplicationBodyComponent } from './application-body/application-body.component';
 
 
 const routes: Routes = [
-  {path:"",component:LoginComponent}
+  {path:"",component:LoginComponent},
+  {path:"home",component:ApplicationBodyComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ApplicationBodyComponent } from './application-body/application-body.component';
+import { NavigationBarComponent } from './application-body/navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ApplicationBodyComponent
+    ApplicationBodyComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
