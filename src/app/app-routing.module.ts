51395e8132736a90b81861cd6c8a64df677cfb90
Maciek Golowkin <maciek.golowkin@gmail.com>
2020-06-12 01:45:09 +0200

    Dodanie profilu uzytkownika

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './Components/login/login.component';
 import { ApplicationBodyComponent } from './Components/application-body/application-body.component';
 import { ImageOperationsComponent } from './Components/application-body/image-operations/image-operations.component';
+import { UserInfoComponent } from './Components/application-body/user-info/user-info.component';
 
 const routes: Routes = [
   {path:"",component:LoginComponent},
   {path:"home",component:ApplicationBodyComponent},
+  {path:"user",component:UserInfoComponent},
   {path:"imageoperations",component:ImageOperationsComponent}
 ];
 
