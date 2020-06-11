234db0c38ed34aef34afc575c9d03d0c413b3d03
Maciek Golowkin <maciek.golowkin@gmail.com>
2020-06-11 17:12:51 +0200

    Dodanie widoku wgrania obrazu

--- src/app/Components/application-body/image-operations/image-operations.component.ts
+++ src/app/Components/application-body/image-operations/image-operations.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-image-operations',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ImageOperationsComponent implements OnInit {
 
-  constructor() { }
+  isDone:boolean=false;
+  fileData: File = null;
+  previewUrl:any = null;
+  fileUploadProgress: string = null;
+  uploadedFilePath: string = null;
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
+  fileProgress(fileInput: any) {
+    this.fileData = <File>fileInput.target.files[0];
+    this.preview();
+}
 
+preview() {
+  // Show preview 
+  var mimeType = this.fileData.type;
+  if (mimeType.match(/image\/*/) == null) {
+    return;
+  }
+
+  var reader = new FileReader();      
+  reader.readAsDataURL(this.fileData); 
+  reader.onload = (_event) => { 
+    this.previewUrl = reader.result; 
+  }
+  this.isDone=true;
+}
+ 
+onSubmit() {
+    const formData = new FormData();
+    formData.append('file', this.fileData);
+    this.http.post('url/to/your/api', formData)
+      .subscribe(res => {
+        console.log(res);
+        // this.uploadedFilePath = res.data.filePath;
+        alert('SUCCESS !!');
+      }) 
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './Components/login/login.component';
@@ -95,6 +95,7 @@ import { ImageOperationsComponent } from './Components/application-body/image-op
     MatTooltipModule,
     MatTreeModule,
     AppRoutingModule,
+    HttpClientModule,
     BrowserAnimationsModule
   ],
   providers: [],
