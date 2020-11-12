53986b24bdb3acdf0b4b5813449161b7fc010cd4
Maciek Golowkin <maciek.golowkin@gmail.com>
2020-11-12 01:58:19 +0100

    Modyfikacja przetwarzania danych

--- src/app/Components/application-body/registry/rows-result/rows-result.component.ts
+++ src/app/Components/application-body/registry/rows-result/rows-result.component.ts
@@ -29,15 +29,20 @@ export class RowsResultComponent implements OnInit {
   async ngOnInit() {
     
     this.listOfImages= await this.imageService.GetArrayOfImages();
-    console.log(this.listOfImages);
+    this.listOfImages.forEach(element => this.rows.push(element, {detailRow: true, element }));
+    this.dataSource= new ExampleDataSource(this.listOfImages);
+    // console.log(this.rows);
 
   }
   xa:any;
   listOfImages:ImageRecord[];
-  displayedColumns = ['position', 'title', 'dateOfUpload'];
-  dataSource = new ExampleDataSource();
+  rows=[];
+  // displayedColumns = ['position', 'name', 'dateOfUpload'];
+  displayedColumns = ['position', 'name'];
+  // dataSource = new ExampleDataSource();
+  dataSource;
   styleOfModal:string;
-  clickedElement:ImageRecordTest;
+  clickedElement:ImageRecord;
   errorMessage:string
   thumbnail;
 
@@ -55,13 +60,22 @@ isExpansionDetailRow = (i: number, row: Object) => row.hasOwnProperty('detailRow
     this.styleOfModal="none";
   }
 
-  cellClicked(element:ImageRecordTest) {
-    this.clickedElement =  ELEMENT_DATA.find(x => x.position == element.position);
+  cellClicked(element:ImageRecord) {
+    console.log(element)
+    this.clickedElement =  this.listOfImages.find(x => x.position == element.position);
   }
 
   deleteElement(id:number) {
-    ELEMENT_DATA.splice(id, 1);
+    this.listOfImages.splice(id, 1);
   }
+
+  connect(): Observable<Element[]> {
+    const rows = [];
+    ELEMENT_DATA.forEach(element => rows.push(element, {detailRow: true, element }));
+    return of(rows);
+  }
+
+  
 }
 
 export interface ImageRecordTest {
@@ -71,34 +85,14 @@ export interface ImageRecordTest {
   description?:string;
 }
 
-const ELEMENT_DATA: ImageRecordTest[] = [
-  {position: 1, title: 'Zdjecie 1', dateOfUpload: new Date("2019-01-16"),description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"},
-  {position: 2, title: 'Zdjecie 2', dateOfUpload: new Date("2019-01-17"),description:"Opissssssssss,"},
-  {position: 3, title: 'Zdjecie aaa', dateOfUpload: new Date("2019-01-18"),description:"Halo halo"},
-  {position: 4, title: 'Zdjecie Medyczne', dateOfUpload: new Date("2019-01-19"),description:"Loreaasdsdadmmy text of the assadas industry's standard dummy text ever since the 1500s,"},
-  {position: 5, title: 'Zdjecie Medyczne Bardzo', dateOfUpload: new Date("2019-01-20")},
-  {position: 6, title: 'Zdjecie obrazo', dateOfUpload: new Date("2019-01-21")},
-  {position: 7, title: 'ziarniaki', dateOfUpload: new Date("2019-01-21")},
-  {position: 8, title: 'Diplococcus', dateOfUpload: new Date("2019-01-22")},
-  {position: 9, title: 'Tetracoccus', dateOfUpload: new Date("2019-01-23")},
-  {position: 10, title: 'Streptococcus', dateOfUpload: new Date("2019-01-24")},
-  {position: 11, title: 'Staphylococcus', dateOfUpload: new Date("2019-01-25")},
-  {position: 12, title: 'Sarcina', dateOfUpload: new Date("2019-08-18")},
-  {position: 13, title: 'Bacterium', dateOfUpload: new Date("2019-07-18")},
-  {position: 14, title: 'Silicon', dateOfUpload: new Date("2019-06-18")},
-  {position: 15, title: 'Bacillus', dateOfUpload: new Date("2019-05-18")},
-  {position: 16, title: 'Corynebacterium', dateOfUpload: new Date("2019-04-18")},
-  {position: 17, title: 'Vibrio', dateOfUpload: new Date("2019-03-18")},
-  {position: 18, title: 'Spirillum', dateOfUpload: new Date("2019-02-18")},
-  {position: 19, title: 'Spirochaeta',dateOfUpload: new Date("2019-07-18")},
-  {position: 20, title: 'Actinomyces',dateOfUpload: new Date("2019-07-19")},
-];
-
 export class ExampleDataSource extends DataSource<any> {
+  constructor(private records: ImageRecord[]){
+    super();
+  }
   /** Connect function called by the table to retrieve one stream containing the data to render. */
   connect(): Observable<Element[]> {
     const rows = [];
-    ELEMENT_DATA.forEach(element => rows.push(element, {detailRow: true, element }));
+    this.records.forEach(element => rows.push(element, {detailRow: true, element }));
     return of(rows);
   }
 
